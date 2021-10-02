1f4301485f927bea8c16972065dc9fcfeb5317a4
Alvin <wangalvin23@gmail.com>
2021-10-02 14:36:37 -0700

    second commit

--- lunarLine.py
+++ lunarLine.py
@@ -5,7 +5,5 @@ def title():
     print(" / /___/ /_/ / / / / /_/ / /     / /___/ / / / /  __/")
     print("/_____/\__,_/_/ /_/\__,_/_/     /_____/_/_/ /_/\___/ ")
     print()
-    print(" HELLO PLEASE WORK GOD DAM IT")
-    print()
-
+    
 title()
