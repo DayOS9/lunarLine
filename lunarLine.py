790c38acbdfbadec813c4121340d753cb134f981
Alvin <wangalvin23@gmail.com>
2021-10-02 18:40:20 -0700

    test commit for north america

--- lunarLine.py
+++ lunarLine.py
@@ -14,28 +14,30 @@ def title():
 
 def diagram(continent):
     if continent.lower() == "north america":
-        print("                   ooo OOO OOO ooo")
-        print("               oOO           _ _ _ OOo")
-        print("           oOO ' ' ,      ,-'          OOo")
-        print("        oOO         '.   :                OOo")
-        print("      oOO   (X)      ;  ,'  ,--.        .-' OOo")
-        print("    oOO            ,'   `--'   `----. ,'      OOo                                      , - ~ ~ ~ - ,")
-        print("   oOO       _ , '    ,-------.      `.        OOo                                 , ' '   '  , .-.  ' ,")
-        print("  oOO     ,'        ,'         `..   .'         OOo                              ,    ' ()  '  (   ) '  ,")
-        print(" oOO  ,- '         :              `-',--.        OOo                            , '  '  '    '  '-'  '   ,")
-        print(" oOO. :        _    `-_ _          ,'    `.      OOo                           ,  ' ()   ' o    '  , o  , ,")
-        print(" oOO `-`- _-,'   `.       `.       :        `----OOo  >>>>>>>>>>>>>>>>>>>>>>>  , '  '   '  '     ()    '  ,")
-        print(" oOO    .'         `.      :       ;             OOo                           ,  '   '  .--.  '   '   o  ,")
-        print(" oOO   :             :     :     ,'        ,-.   OOo                            ,   o   |    |  '  o   , ,")
-        print("  oOO   :.           ;     :   ,'       _,'   `-OOo                              , '  ' `.__.'  '   '   ,")
-        print("   oOO    :        ,'      `..'      ,'        OOo                                 , '  '  ,  ' () ' , '")
-        print("    oOO  :      ,'                  :         OOo                                    ' - , _ _ _ , '")
-        print("      oOO`    ,'                     `.     OOo")
-        print("        oOO ,'    . - - - - - - - .    `. OOo")
-        print("           oOO','                   `, OOo")
-        print("               oOO                 OOo")
-        print("                   ooo OOO OOO ooo")
-    
+        a = "                   ooo OOO OOO ooo\n"
+        b = "               oOO           _ _ _ OOo\n"
+        c = "           oOO ' ' ,      ,-'          OOo\n"
+        d = "        oOO         '.   :                OOo\n"
+        e = "      oOO   (X)      ;  ,'  ,--.        .-' OOo\n"
+        f = "    oOO            ,'   `--'   `----. ,'      OOo                                      , - ~ ~ ~ - ,\n"
+        g = "   oOO       _ , '    ,-------.      `.        OOo                                 , ' '   '  , .-.  ' ,\n"
+        h = "  oOO     ,'        ,'         `..   .'         OOo                              ,    ' ()  '  (   ) '  ,\n"
+        i = " oOO  ,- '         :              `-',--.        OOo                            , '  '  '    '  '-'  '   ,\n"
+        j = " oOO. :        _    `-_ _          ,'    `.      OOo                           ,  ' ()   ' o    '  , o  , ,\n"
+        k = " oOO `-`- _-,'   `.       `.       :        `----OOo  >>>>>>>>>>>>>>>>>>>>>>>  , '  '   '  '     ()    '  ,\n"
+        l = " oOO    .'         `.      :       ;             OOo                           ,  '   '  .--.  '   '   o  ,\n"
+        m = " oOO   :             :     :     ,'        ,-.   OOo                            ,   o   |    |  '  o   , ,\n"
+        n = "  oOO   :.           ;     :   ,'       _,'   `-OOo                              , '  ' `.__.'  '   '   ,\n"
+        o = "   oOO    :        ,'      `..'      ,'        OOo                                 , '  '  ,  ' () ' , '\n"
+        p = "    oOO  :      ,'                  :         OOo                                    ' - , _ _ _ , '\n"
+        q = "      oOO`    ,'                     `.     OOo\n"
+        r = "        oOO ,'    . - - - - - - - .    `. OOo\n"
+        s = "           oOO','                   `, OOo\n"
+        t = "               oOO                 OOo\n"
+        u = "                   ooo OOO OOO ooo\n"
+        diagram = [a, b, c ,d ,e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u]
+        return diagram
+
     if continent.lower() == "south america":
         print("                   ooo OOO OOO ooo")
         print("               oOO           _ _ _ OOo")
@@ -175,4 +177,4 @@ def diagram(continent):
         print("                   ooo OOO OOO ooo")
 
 title()
-diagram("africa")
+print(diagram("north america"))
